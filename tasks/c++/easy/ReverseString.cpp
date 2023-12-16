096b8f85966b492d86df52cb3b77baccdbae07d7
mukul-pixel <140041186+mukul-pixel@users.noreply.github.com>
2023-12-16 15:16:57 +0530

    Update ReverseString.cpp
    completed the task to reverse the string.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -3,9 +3,26 @@
 
 std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
+    std::string reversedStr;
+
+    // Iterate through the characters of the input string in reverse order
+    for (int i = str.length() - 1; i >= 0; --i) {
+        // Append each character to the reversed string
+        reversedStr += str[i];
+    }
+
+    // Return the reversed string
+    return reversedStr;
 }
 
 int main() {
     // TODO: Implement the main method
+    std::string str;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+
+    // Calling the reverseString function and displaying the result
+    std::string reversed = reverseString(str);
+    std::cout << "Reversed string: " << reversed << std::endl;
 }
 
